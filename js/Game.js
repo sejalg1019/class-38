f188f3e6cde7b2b71544968b33f0b43483f49b8c
sejalg1019 <sejalg1019@gmail.com>
2020-12-15 19:49:52 -0500

    files-added

--- js/Game.js
+++ js/Game.js
@@ -26,31 +26,43 @@ class Game {
       form = new Form()
       form.display();
     }
+    car1 = createSprite(100,200);
+    car2 = createSprite(300,200);
+    car3 = createSprite(500,200);
+    car4 = createSprite(700,200);
+
+    cars = [car1,car2,car3,car4];
   }
 
   play(){
     form.hide();
-    textSize(30);
-    text("Game Start", 120, 100)
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
-      var display_position = 130;
+      //var display_position = 130;
+      var index = 0;
+      var x = 0;
+      var y;
+      
       for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
-        else
-          fill("black");
-
-        display_position+=20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        
+        index+=1; //incrementing the index everytime a player joins
+        x+=200; //creating space between the cars
+        y = displayHeight-allPlayers[plr].distance; //using the data form the database to display the cars in y direction
+        cars[index-1].x = x; //as the index changes, the x position will decrease by one
+        cars[index-1].y = y; //as the index changes, the y position will decrease by one
+        
+        if(index === player.index){
+          cars[index-1].shapeColor = "red"; 
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y;
+        }
       }
     }
-
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=50
-      player.update();
-    }
+        if(keyIsDown(UP_ARROW)&& player.index !== null){
+          player.distance+=10;
+          player.update();
+        }
+        drawSprites();
   }
 }
